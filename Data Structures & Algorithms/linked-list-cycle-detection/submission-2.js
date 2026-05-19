1182fae5ba572235045c6f5a483821d753814201
Luis <53343656+Lucho2027@users.noreply.github.com>
2026-05-18 20:34:33 -0500

    Add: linked-list-cycle-detection - submission-2

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-2.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {boolean}
+     */
+    hasCycle(head) {
+       let slow = head;
+       let fast = head;
+       let result = false
+       while(fast !== null && fast.next !== null){
+            slow = slow.next;
+            fast = fast.next.next
+        if(slow === null || fast === null) {
+            break
+        }
+        if(slow === fast){
+            result = true
+           break 
+        }
+       }
+       console.log(result)
+       return result
+    }
+}
